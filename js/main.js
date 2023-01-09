9c4a9028c85267541867519483690d39e5ae179d
Silvia0307 <sonorous0307@gmail.com>
2023-01-10 00:38:44 +0900

    Main > characters 완료
    - Main > characters : Markup, Style 완료
    - Main > characters :  캐릭터 설명 기능 추가(js)

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
+'use strict'; // 엄격모드on
+
+// Main > Characters 버튼 클릭 이벤트
+// Main > Characters 버튼 클릭하면 연관된 이미지와 콘텐츠를 보여줌
 const controlBtn = document.querySelectorAll('.control_btn');
 const controlImg = document.querySelectorAll('.characters_img > img');
+const controlContent = document.querySelectorAll('.control_content');
 
-controlBtn.forEach((controlButton) => {
-    controlButton.addEventListener('click',()=>{
-        // console.log(controlButton.name);
+controlBtn.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        // 이미지 보여주기
         controlImg.forEach((viewImg)=>{
-            if(controlButton.name == viewImg.id){
+            if(button.name == viewImg.id){
                 viewImg.classList.remove('active');
                 viewImg.classList.add('active');
-                // console.log(controlButton.name,viewImg.id);
             }else{
                 viewImg.classList.remove('active');
-            }
+            } 
+        });
+        // 콘텐츠 보여주기
+        controlContent.forEach((viewContent)=>{
+            if(button.name == viewContent.dataset.content){
+                viewContent.classList.remove('active');
+                viewContent.classList.add('active');
+            }else{
+                viewContent.classList.remove('active');
+            } 
         });
     });
 });
+
